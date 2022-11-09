ba103907077001bd0df25ecfb70cba101daabeb7
George Ngigi <ngigegeorge023@gmail.com>
2022-11-09 23:03:46 +0300

    removed validation since it has been done in the client side

--- login.php
+++ login.php
@@ -4,21 +4,17 @@ include("est_connection.php");
 $username = $_POST['username']; // the name to the input text box
 $password = $_POST['password']; // the name to the input password textbox
 
-if (strlen($username) != 0 || strlen($password) != 0) {
-	$query = "SELECT * FROM logins WHERE username = '$username' AND password = '$password';";
+$query = "SELECT * FROM logins WHERE username = '$username' AND password = '$password';";
 
-	$result = mysqli_query($connection, $query);
-	$counter = mysqli_fetch_object($result);
-	if (mysqli_num_rows($result) > 0) {
-		// take the user to the homepage later, it should return the user the previous page before ligin
-		// the name should also be coockied and later be used to set the username details throughout the sessions
-		header("Location: realest.html");
-		die();
-	} else {
-		// return the user to the login page
-		header("Location: login.html");
-		die();
-	}
+$result = mysqli_query($connection, $query);
+$counter = mysqli_fetch_object($result);
+if (mysqli_num_rows($result) > 0) {
+	// take the user to the homepage later, it should return the user the previous page before ligin
+	// the name should also be coockied and later be used to set the username details throughout the sessions
+	header("Location: realest.html");
+	die();
 } else {
+	// return the user to the login page
 	header("Location: login.html");
+	die();
 }
